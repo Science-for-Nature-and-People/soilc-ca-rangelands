e869dfc0bcf4216d231cae85d591586d7f78e89f
Dillon Osleger <djosleger@gmail.com>
2018-05-23 09:21:19 -0700

    data update

---
+++ food-web-metaanalysis/code/simplified_rmz_plotting.R
@@ -0,0 +1,27 @@
+install.packages(dismo)
+
+library(dismo)
+library(rgdal)
+library(raster)
+library(sf)
+
+x <- raster("~/Desktop/RMZ-raster/RMZ.tif", package="raster")
+x
+df <- read.csv("~/Desktop/sites.csv")
+sites <- st_as_sf(df,
+                  coords = c('x', 'y'),
+                  crs = "+proj=longlat +datum=WGS84 +no_defs")
+sites_sp <- as(sites, "Spatial")
+
+#projection(x) <- CRS('+proj=longlat +datum=WGS84 +no_defs')
+sites.mrc <- spTransform(sites_sp, CRS(projection(x)))
+data <- extract(x, sites.mrc, method="simple", buffer=1000, fun=median)
+sites$rmz <- data
+data
+
+
+plot(x)
+plot(sites.mrc,add=TRUE, col="saegreen2", pch = 18)
+
+
+df$RMZ <- data
