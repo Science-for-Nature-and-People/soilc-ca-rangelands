40e4067f68f22ae531afda7e8515c6ff3994800e
Kelly Gravuer <Kelly@Kellys-MacBook-Pro-3.local>
2018-07-27 12:47:22 -0700

    Added code for outcome forest plots

--- food-web-metaanalysis/code/logic_chain.R
+++ food-web-metaanalysis/code/logic_chain.R
@@ -111,7 +111,11 @@ soilp.all$order <- ifelse(soilp.all$practice_predictor=="tree/shrub presence", 4
                                  ifelse(soilp.all$practice_predictor=="organic amendment", 2, 
                                         ifelse(soilp.all$practice_predictor=="riparian restoration", 1, NA))))
 
-q=ggplot(soilp.all, aes(y=order, x=lnRR, xmin=lnRR.ci.lower, xmax=lnRR.ci.upper))+
+
+setwd("~/Documents/Kelly/NatureNet_Postdoc/soilc-ca-rangelands/food-web-metaanalysis/figures/")
+
+pdf("forest_plot_soil_properties.pdf")
+ggplot(soilp.all, aes(y=order, x=lnRR, xmin=lnRR.ci.lower, xmax=lnRR.ci.upper))+
   #Add data points
   geom_point(data=subset(soilp.all, practice_predictor=='tree/shrub presence'), color='green', size=4) +
   geom_point(data=subset(soilp.all, practice_predictor=='grazing'), color='blue', size=4) +
@@ -128,6 +132,120 @@ q=ggplot(soilp.all, aes(y=order, x=lnRR, xmin=lnRR.ci.lower, xmax=lnRR.ci.upper)
   geom_vline(xintercept=0, color='black', linetype='dashed')+
   facet_grid(property_response~., scales= 'fixed', space='fixed', as.table=T)
 
-q
+dev.off()
 
-ggsave("forest_plot_soil_properties.pdf", width=7, height=8.1)
+##### MANAGEMENT - OUTCOME RELATIONSHIPS #####
+
+# subset to look just at outcome - management relationships
+out.temp <- d[d$response_type == "outcome",]
+
+# exclude rows for which yi and vi cannot be calculated
+outcomes <- out.temp[is.na(out.temp$control_mean)==F & is.na(out.temp$control_sd)==F & is.na(out.temp$control_n)==F & is.na(out.temp$treatment_mean)==F & is.na(out.temp$treatment_sd)==F & is.na(out.temp$treatment_n)==F,]
+
+outcomes$response <- factor(outcomes$response)
+outcomes$response_lumped <- factor(outcomes$response_lumped)
+
+a <- as.data.frame(table(outcomes$response, outcomes$response_lumped))
+b <- dcast(a, Var1 ~ Var2)
+names(b)[1] <- "response"
+names(b)[2:7] <- gsub(" ",".",names(b)[2:7])
+
+forage.production <- b[b$forage.production > 0,c("response","forage.production")]
+native.plant.abundance <- b[b$native.plant.abundance > 0,c("response","native.plant.abundance")]
+natural.pest.control <- b[b$natural.pest.control > 0,c("response","natural.pest.control")]
+plant.diversity <- b[b$plant.diversity > 0,c("response","plant.diversity")]
+plant.tissue.quality <- b[b$plant.tissue.quality > 0,c("response","plant.tissue.quality")]
+water.quality <- b[b$water.quality > 0,c("response","water.quality")]
+
+forage.production
+native.plant.abundance
+natural.pest.control
+plant.diversity
+plant.tissue.quality
+water.quality
+
+# pH has site-specific interpretation in terms of whether higher pH makes water quality "better" or "worse"
+# DO is the only water quality response for which higher values are better
+# OMIT both of these for now, for simplicity
+
+outcomes <- outcomes[ !(outcomes$response %in% c("DO (Spring water)","pH (spring water)")) , ]
+
+outcomes$treatment_category_1 <- factor(outcomes$treatment_category_1)
+outcomes$ref_code <- factor(outcomes$ref_code)
+outcomes$response <- factor(outcomes$response)
+outcomes$response_lumped <- factor(outcomes$response_lumped)
+
+# calculate yi and vi for each row of data
+res <- escalc(measure="ROM", m1i=treatment_mean, sd1i=treatment_sd, n1i=treatment_n, m2i=control_mean, sd2i=control_sd, n2i=control_n, data=outcomes)
+
+##### START HERE - some have vi = NA, check
+
+# analyze only those relationships that have 3 or more observations
+outctab <- as.data.frame(table(outcomes$response_lumped, outcomes$treatment_category_1))
+outcan <- outctab[outctab$Freq >= 3,c(2,1)]
+names(outcan) <- c("practice_predictor","outcome_response")
+
+# create empty data frame for effect size calculation output
+outcomes.res <- data.frame(lnRR=numeric(), lnRR.ci.lower=numeric(), lnRR.ci.upper=numeric(), n.obs=integer(), n.papers=integer())
+
+# loop through outc property - management pairs that have 3 or more observations and calculate effect sizes
+for (i in 1:nrow(outcan)){
+  dsub <- res[res$treatment_category_1 == outcan$practice_predictor[i] & res$response_lumped == outcan$outcome_response[i],]
+  dsub$ref_code <- factor(dsub$ref_code)
+  m1 <- rma.mv(yi, vi, random = ~ 1 | ref_code, method="ML", data=dsub)
+  outcomes.res[i,"lnRR"] <- coef(m1)
+  outcomes.res[i,"lnRR.ci.lower"] <- m1$ci.lb
+  outcomes.res[i,"lnRR.ci.upper"] <- m1$ci.ub
+  outcomes.res[i,"n.obs"] <- nrow(dsub)
+  outcomes.res[i,"n.papers"] <- nlevels(dsub$ref_code)
+}
+
+# make forest plot
+outcomes.all <- cbind(outcan, outcomes.res)
+
+# make name shorter to fit in label box
+outcomes.all$outcome_response <- gsub("native plant abundance","native plants", outcomes.all$outcome_response)
+# change name to indicate directionality: higher values are bad
+outcomes.all$outcome_response <- gsub("water quality","water pollution", outcomes.all$outcome_response)
+
+outcomes.all$order <- ifelse(outcomes.all$practice_predictor=="tree/shrub presence", 3, 
+                          ifelse(outcomes.all$practice_predictor=="grazing", 2, 
+                                 ifelse(outcomes.all$practice_predictor=="organic amendment", 1, NA)))
+
+setwd("~/Documents/Kelly/NatureNet_Postdoc/soilc-ca-rangelands/food-web-metaanalysis/figures/")
+
+pdf("forest_plot_outcomes.pdf")
+ggplot(outcomes.all, aes(y=order, x=lnRR, xmin=lnRR.ci.lower, xmax=lnRR.ci.upper))+
+  #Add data points
+  geom_point(data=subset(outcomes.all, practice_predictor=='tree/shrub presence'), color='green', size=4) +
+  geom_point(data=subset(outcomes.all, practice_predictor=='grazing'), color='blue', size=4) +
+  geom_point(data=subset(outcomes.all, practice_predictor=='organic amendment'), color='red', size=4) +
+  #add the CI error bars
+  geom_errorbarh(height=.1)+ theme_bw()+ 
+  #Specify the limits of the x-axis and relabel it to something more meaningful
+  scale_x_continuous(limits=c(-6,6), name='Log Response Ratio')+
+  scale_y_continuous(limits=c(0,5), breaks = outcomes.all$order, labels = outcomes.all$practice_predictor) + 
+  theme(axis.text.y=element_text(size=13), axis.text.x=element_text(size=13), axis.title.x=element_text(size=15), strip.text.y = element_text(size=13))+ 
+  ylab('')+
+  #Add a vertical dashed line indicating an effect size of zero, for reference
+  geom_vline(xintercept=0, color='black', linetype='dashed')+
+  facet_grid(outcome_response~., scales= 'fixed', space='fixed', as.table=T)
+
+dev.off()
+
+
+##### MANAGEMENT-SOIL PROPERTY AND MANAGEMENT-OUTCOME RELATIONSHIPS IN SAME PAPER? #####
+
+d$management.ref <- paste0(d$treatment_category_1,".",d$ref_code)
+c <- as.data.frame(table(d$management.ref, d$response_type))
+e <- dcast(c, Var1~Var2)
+e[e$outcome > 0 & e$soil > 0,]
+
+#                                               Var1 outcome soil
+#        grazing.Stahlheber_D'Antonio_and_Tyler_2016       1    4
+#  tree/shrub presence.Stahlheber_and_D'Antonio_2014       8    4
+
+# Just two papers measured BOTH soil property and outcome changes in response to the same management.
+# So we can't correlated the lnRRs as Chelsea and I had hoped, at least with the current data set.
+
+
