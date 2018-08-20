241b5b7642e0a9ab02684ec2729db06511a2327e
Stephen Wood <stephenawood@gmail.com>
2018-08-20 13:22:23 -0700

    Loaded admin shapefiles

--- rangeland-production/raster_processing/NDVI_code.R
+++ rangeland-production/raster_processing/NDVI_code.R
@@ -24,6 +24,7 @@ rangeland_in <- file.path(path_input, rangeland_filename)
 ## Define the projection to use (ESPG 3310: NAD 83 California Albers)
 
 newproj <- "+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs" 
+newproj <- "+proj=aea +ellps=GRS80 +datum=NAD83 +units=m +no_defs" 
 
 ## Output filename
 
@@ -33,19 +34,25 @@ output_name <- "RMZ_NDVI_Mean_Ratio"
 ##### MAIN ##### 
 
 ## Load raster data
-
 NDVI <- raster(ca_ndvi)
 rangelands <- raster(rangeland_in)
 
-## Reproject and resample data
+## Load shapefiles
+state <- file.path(path_input,"/soilc-california/rangeland-production/data/shapefiles/Admin/CA_State_TIGER2016.shp") %>%
+  rgdal::readOGR()
+county <- file.path(path_input,"/soilc-california/rangeland-production/data/shapefiles/Admin/CA_Counties_TIGER2016.shp") %>%
+  rgdal::readOGR()
 
+## Reproject and resample data
 proj <- projectExtent(rangelands, crs = newproj)  # Set a new reprojection (blank raster)
 range_proj <- projectRaster(rangelands, proj, method= "ngb", alignOnly = FALSE) # Apply new projection to rangelands data
 NDVI_proj <- projectRaster(NDVI, range_proj, method = "ngb", alignOnly = FALSE) # Project, align, and crop the NDVI layer to the projection
 
+## Plot data
+plot(NDVI_proj)
+plot(county, add=T)
 
 ## Create raster of mean values
-
 mean_NDVI <- zonal(NDVI_proj, range_proj, 'mean', na.rm = TRUE) # This creates a matrix of mean values for each RMZ (median only works for smaller rasters)
 mean_NDVI_df <- as.data.frame(mean_NDVI) # Converts into dataframe, col 1 shows RMZ value (1-6), col 2 shows corresponding mean value
 mean_raster <- subs(range_proj, mean_NDVI_df, by=1, which=2, subsWithNA = TRUE) # Uses dataframe to reclassify raster (replace values in column 1 with matching values in column 2)
